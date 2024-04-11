fc58108e7a87d1154782c20c9496a12cb18a72ca
Rafli Ferdian <raflitsany35@gmail.com>
2024-04-11 20:14:28 +0700

    fix: validate empty value

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,10 @@
 export const validateString = (value: unknown, maxLength: number) => {
-  if (!value || typeof value !== "string" || value.length > maxLength) {
+  if (
+    !value ||
+    typeof value !== "string" ||
+    value.length > maxLength ||
+    value.trim().length === 0
+  ) {
     return false;
   }
   return true;
